d82b265c0a96918fa1e9d901d019870c6dbd4261
mymrtt <mymrtt@gmail.com>
2020-08-27 16:29:10 -0300

    Updates

---
+++ precache-manifest.c3578ddfc6f2f02180df019bc4e79bc0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f9f0b773a289a67249bd01e7e5b11cd2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8416dbc6d0801e934ff9",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "d3d80b605e8ae4ab53fa",
+    "url": "/static/js/2.115e08af.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.115e08af.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8416dbc6d0801e934ff9",
+    "url": "/static/js/main.4a8d01ea.chunk.js"
+  },
+  {
+    "revision": "3eb330cbf81ebd8a03d0",
+    "url": "/static/js/runtime-main.d6e02a3b.js"
+  }
+]);
